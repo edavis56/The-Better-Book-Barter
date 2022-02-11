089643a63e3c4680b1a7bd3a7f01d66d686aafa9
Austin Wonka <wonkaaustin@gmail.com>
2022-02-10 20:24:14 -0500

    adding bookroutes

---
+++ controllers/api/bookRoutes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { User, Book } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get functions
+// render all books to inventory page
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const bookData = await Book.findAll();
+
+        const books = bookData.map((book) => book.get({ plain: true }));
+
+        res.render('inventory', {
+            books,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// when selecting a specific book
+router.get('/book/:id', withAuth, async (req, res) => {
+    try {
+        const bookData = await Book.findByPk(req.params.id);
+        // May want to include user data here?
+
+        if (!bookData) {
+            res.status(404).json({ 
+                message: "Book not found!"
+            });
+            return;
+        }
+
+        const book = bookData.get({ plain: true });
+        console.log(book);
+
+        res.render('book', {
+            ...book,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create, update, and delete entries
+// book submission
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newBook = await Book.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBook);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// update book data
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const bookData = await Book.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!bookData) {
+            res.status(404).json({ message: "No book found with this id!" });
+            return;
+        }
+
+        res.status(200).json(bookData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// delete book data, probably don't need
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const bookData = await Book.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!bookData) {
+            res.status(404).json({ message: "No book found with this id!" });
+            return;
+        }
+
+        res.status(200).json(bookData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
