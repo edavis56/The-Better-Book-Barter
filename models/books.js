f06e760d9c2d949a1c8ae159b57330c933808f3d
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-02-12 11:34:36 -0500

    Update to db and added server.js file

--- models/books.js
+++ models/books.js
@@ -5,12 +5,12 @@ class books extends Model {}
 
 books.init(
   {
-    // id: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   primaryKey: true,
-    //   autoIncrement: true,
-    // },
+     id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true,
+     },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -31,7 +31,24 @@ books.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    ranking:
+    ranking: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    donor_username: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'user',
+        key: 'username',
+      },
+    rec_username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'username',
+      },
+    },
   },
   {
     sequelize,
