e0e016b793539bf2f9754a293265e5e89f8270e2
rhoverholt <rhoverholt@gmail.com>
2022-02-12 13:33:12 -0500

    integrate controller and view for mybookshelf and submit book page

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,67 @@ const router = require("express").Router();
 // const apiRoutes = require('./api');
 // const homeRoutes = require('./home-routes.js');
 
+router.get("/user/:id", (req, res) => {
+  let username = "Rich Overholt";
+
+  let donatedBooks = [
+    {
+      date: "02/10/2022",
+      title: "This is the title of my most recent book",
+      author: "LastName, FirstName",
+    },
+    {
+      date: "02/05/2022",
+      title: "This is the title of my penultimate book",
+      author: "LastName, FirstName",
+    },
+    {
+      date: "02/01/2022",
+      title: "This is the title of my first book",
+      author: "LastName, FirstName",
+    },
+  ];
+
+  let receivedBooks = [
+    {
+      date: "02/10/2022",
+      title: "This is the only book I've checked out",
+      author: "LastName, FirstName",
+    },
+  ];
+
+  let donatedCount = donatedBooks.length;
+  let receivedCount = receivedBooks.length;
+  let availableCount = donatedCount - receivedCount;
+
+  let rankings = [
+    {
+      info: `# of books donated: ${donatedCount} (5th / 17 users)`,
+    },
+    {
+      info: `# of books received: ${receivedCount} (16th / 17 users)`,
+    },
+    {
+      info: `Average rating of books donated: 3.26 (5th / 17 users)`,
+    },
+  ];
+
+  res.render("myBookshelf", {
+    username,
+    donatedBooks,
+    receivedBooks,
+    donatedCount,
+    receivedCount,
+    availableCount,
+    rankings,
+    loggedIn: true,
+  });
+});
+
+router.get("/submit", (req, res) =>
+  res.render("submitBook", { loggedIn: true })
+);
+
 router.get("*", (req, res) =>
   res.status(200).send("Routes are currently under development")
 );
