d804aa3fc8d2eadee39e8748c3edcb25253acb4b
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-02-12 11:51:14 -0500

    Update to server.js

---
+++ models/genre.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class genre extends Model {}
+
+books.init(
+  {
+     id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true,
+     },
+     type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'books',
+  },
+);
+
+module.exports = genre;
--- server.js
+++ server.js
@@ -23,7 +23,9 @@ const sess = {
     }),
   };
 
-  app.use(session(sess));
+app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
