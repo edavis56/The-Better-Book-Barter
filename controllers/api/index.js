fc36c88ee6652f22cf799e3440bba19d926382b1
rhoverholt <rhoverholt@gmail.com>
2022-02-15 20:08:43 -0500

    start controler structure

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const bookRoutes = require("./bookRoutes");
+const userRoutes = require("./userRoutes");
+
+router.use("/books", bookRoutes);
+router.use("/users", userRoutes);
