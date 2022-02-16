7679580b3f464dacb65e3d99e145c2e76d005d28
rhoverholt <rhoverholt@gmail.com>
2022-02-15 19:53:15 -0500

    implement and test model changes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,24 @@
-const User = require('./User');
-const Painting = require('./books');
+const User = require("./User");
+const Book = require("./Book");
+const Condition = require("./Condition");
+const Genre = require("./Genre");
 
-module.exports = { User, books };
+Book.belongsTo(User, {
+  foreignKey: "donor_id",
+});
+
+User.hasMany(Book, {
+  foreignKey: "donor_id",
+  onDelete: "SET NULL",
+});
+
+Book.belongsTo(User, {
+  foreignKey: "rec_id",
+});
+
+User.hasMany(Book, {
+  foreignKey: "rec_id",
+  onDelete: "SET NULL",
+});
+
+module.exports = { User, Book, Condition, Genre };
