404fed56c322833f72cc4112a5188cc9dda42981
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-02-17 19:46:02 -0500

    Added admin api, handlebar, and html mockup

---
+++ controllers/api/adminRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const { Genre, Condition } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// Create genre
+router.post("/admin", (req, res) => {
+  Genre.create({
+    name: req.body.name,
+  }).then((genreData) => {
+    req.session.save(() => {
+      req.session.name = genreData.name;
+
+      res.json(genreData);
+    });
+  });
+});
+
+// Create condition
+router.post("/admin", (req, res) => {
+  Condition.create({
+    state: req.body.name,
+  }).then((conditionData) => {
+    req.session.save(() => {
+      req.session.state = conditionData.name;
+
+      res.json(conditionData);
+    });
+  });
+});
+
+module.exports = router;
