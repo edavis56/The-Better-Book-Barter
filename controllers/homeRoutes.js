40dc4cf4887ea7a0ba902834199a4e0c216f886b
Jude Collins <jude.collins13@yahoo.com>
2022-02-17 20:03:28 -0500

    working on controllers for login/logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,15 @@ const applyOrdinalSuffix = require("./../utils/aos");
 
 const router = require("express").Router();
 
+//login dont know if this will work
+router.get("/login", (req, res) => {
+  //  if (req.session.loggedIn) {
+  //     res.redirect('/');
+  //     return;
+  //   }
+  res.render("login", { loggedIn: false });
+});
+
 router.get("/", (req, res) => res.render("homepage", { loggedIn: true }));
 
 router.get("/bookshelf", async (req, res) => {
