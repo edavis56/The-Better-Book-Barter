5a99587f1c2be616b2b68e76963d9b56cdf1952b
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-02-17 20:02:14 -0500

    update to admin.js file adding logic for submit button

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,10 +1,16 @@
-const genre = document.querySelector('#genre').value 
-const condition = document.querySelector('#condition').value
+async function adminFormhandler(event) {
+    event.preventDefault();
+}
+
+const genre = document.querySelector('#genre').value.trim() 
+const condition = document.querySelector('#condition').value.trim()
 
 if(genre && condition) {
     const response = await fetch("/api/admin", {
         method: "POST",
         body: JSON.stringify({genre, condition}),
-        headers: {""},
+        headers: {'Content-Type': 'application/json'},
     });
 }
+
+document.querySelector('.admin-input').addEventListener('submit', adminFormhandler);
